74928785bf00c8fe341ec41d29b8ac8ad324193c
ahmadreza <amiraligoodboy892@gmail.com>
2024-03-05 17:21:19 -0800

    setup shadcn ui library

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Vazirmatn } from "next/font/google";
 import "../globals.css";
+import ThemeProvider from "@/Providers/NextThemeProvider";
 
 const inter = Inter({ subsets: ["latin"], display: "swap" });
 const vazir = Vazirmatn({ subsets: ["arabic"], display: "swap" });
@@ -12,13 +13,19 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  params: { locale },
 }: Readonly<{
   children: React.ReactNode;
+  params: {
+    locale: string;
+  };
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark" dir={locale === "fa" ? "rtl" : "ltr"}>
       <body className={`${inter.className} ${vazir.className} font-sans`}>
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
