5b278fd12654e8021f8ad0de860cfea45186d0ad
ahmadreza <amiraligoodboy892@gmail.com>
2024-05-21 18:58:02 +0330

    replace emoji-picker-react with emoji mart

---
+++ src/components/EmojiPickerMart.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { memo, useMemo, useRef, useState } from "react";
+
+import Picker from "@emoji-mart/react";
+import data from "@emoji-mart/data";
+import { cn } from "@/lib/utils";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/Popover";
+import { createPortal } from "react-dom";
+import { useClickOutside } from "@mantine/hooks";
+
+interface EmojiPickerProps {
+  onChangeEmoji: (emoji: string) => void;
+  emoji: string;
+  classNames?: string;
+}
+
+const EmojiPickerReact: React.FC<EmojiPickerProps> = ({
+  classNames,
+  emoji,
+  onChangeEmoji,
+}) => {
+  const { theme, systemTheme } = useTheme();
+  const [open, setOpen] = useState(false);
+  const t = useMemo(() => theme || systemTheme, [theme]);
+
+  const onClickEmoji = (e: any) => {
+    console.log(e);
+    onChangeEmoji(e.native);
+    setOpen(false);
+  };
+
+  const wrapperRef = useClickOutside(() => {
+    setOpen(false);
+  });
+
+  return (
+    <div>
+      <p
+        className={cn(`cursor-pointer`, classNames)}
+        onClick={() => setOpen(true)}
+      >
+        {emoji}
+      </p>
+      {createPortal(
+        <div
+          className={cn(
+            "fixed z-[500] inset w-full h-full left-0 top-0 bottom-0 right-0",
+            {
+              hidden: !open,
+              "flex visible": open,
+            }
+          )}
+        >
+          <div className="flex items-center z-[501] justify-center w-full h-full">
+            <div className="w-fit h-fit" ref={wrapperRef}>
+              <Picker
+                emojiSize={24}
+                data={data || []}
+                onEmojiSelect={onClickEmoji}
+                locale="en"
+                noCountryFlags={false}
+                onClickOutside={() => {
+                  if (open) setOpen(false);
+                }}
+              />
+            </div>
+          </div>
+        </div>,
+        document.body
+      )}
+    </div>
+  );
+};
+
+export default memo(EmojiPickerReact);
